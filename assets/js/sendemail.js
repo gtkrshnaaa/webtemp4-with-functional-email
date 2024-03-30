d3f115102e2afa7669072ca9a3f36b874bc8bd3b
Gilang Teja Krishna <114217089+gtkrshnaaa@users.noreply.github.com>
2024-03-30 11:41:42 +0700

    update

---
+++ assets/js/sendemail.js
@@ -0,0 +1,9 @@
+function composeEmail() {
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    const mailtoLink = `mailto:gilangtejakrishnaa@gmail.com?cc=${encodeURIComponent(email)}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+
+    window.location.href = mailtoLink;
+}
